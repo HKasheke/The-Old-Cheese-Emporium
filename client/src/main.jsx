50dc74c535c00392c620ee480c7e9ad9edebf865
HKasheke <hope.kasheke@gmail.com>
2024-11-28 14:26:09 -0400

    Cookies working

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -24,7 +24,7 @@ const router = createBrowserRouter([
         element: <Home />
       },
       {
-        path: "/products/:id",
+        path: "/details/:id",
         element: <Details />
       },
       {
--- client/src/routes/Details.jsx
+++ client/src/routes/Details.jsx
@@ -2,28 +2,42 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Card from "../ui/Card";
 import { Link } from "react-router-dom";
+import { useCookies } from 'react-cookie';
 
-export default function Details() {
+export default function Details(){
   const { id } = useParams();
+
+  const [cookies, setCookie, removeCookie] = useCookies(["cart"]);
 
   const [product, setProduct] = useState(null);
 
   const hostUrl = import.meta.env.VITE_APP_HOST;
   const productUrl = hostUrl + "/api/products/" + id;
+
+  function create (){
+    setCookie("cart", '');
+  }
+
+  function addToCart(cart){
+    if(cookies.cart){
+      setCookie("cart", cookies.cart + ',' + id, {maxAge: 3600});
+    } else {
+      setCookie("cart", id, {maxAge: 3600});
+    }
+  }
+
+  function deleteCookie(){
+    removeCookie('cart');
+  }
   
   useEffect(() => {
     async function fetchProduct() {
-      const response = await fetch(productUrl, {
-        method: 'GET',
-        credentials: 'include'
-      }); 
+      const response = await fetch(productUrl); 
       if(response.ok){
         const data = await response.json();
         if(!ignore){
           setProduct(data);
-          console.log(product);
         } else {
-          console.log("HERE")
           setProduct(null);
         }
       }
@@ -31,7 +45,6 @@ export default function Details() {
 
     let ignore = false;
     fetchProduct();
-    console.log(product);
     return () => {
       ignore = true;
     }
@@ -41,6 +54,12 @@ export default function Details() {
   return (
     <div>
       <h1>Details</h1>
+        <div>
+          { product && <Card product={product} apiHost={hostUrl} showLinks={false}/> }
+        </div>
+        <p className='d-grid gap-2 d-md-flex justify-content-center'>
+          <button className="btn btn-danger btn me-md-2" onClick={addToCart}>Add To Cart</button> <Link to="/home" className="btn btn-outline-secondary ">Cancel</Link>
+        </p>
     </div>
   );
 }
